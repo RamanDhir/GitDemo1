5397667b55e4331fd445cd283bfee9d8a8b4fb5c
raman <raman.dhir1991@gmail.com>
2019-04-08 17:59:42 +0530

    This is commit from User X/Asian guy

--- src/pack1/Class1.java
+++ src/pack1/Class1.java
@@ -5,6 +5,9 @@ public class Class1 {
 	public static void main(String[] args) 
 	{
 		System.out.println("Original print");
+		System.out.println("CHANGES 1 MADE after cloning");
+		System.out.println("CHANGES 2 MADE after cloning");
+
 
 	}
 
